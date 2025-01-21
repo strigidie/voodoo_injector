6ed4736057b62b38df14f7b682029165f65699a8
George Vikulov <strigidie@gmail.com>
2025-01-20 22:50:32 -0800

    init

---
+++ include/voodoo.h
@@ -0,0 +1,7 @@
+#ifndef VOODOO_H
+# define VOODOO_H
+
+#include <stdlib.h>
+
+#endif
+
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include "voodoo.h"
+
+int main(const int argc, const char * const * argv, const char * const * envp)
+{
+    return EXIT_SUCCESS;
+}
+
